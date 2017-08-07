cf64294a8937b4a84189e380b8332cbb8c24a1ca
Luca Carducci <l.carducci@almaviva.it>
2017-08-07 14:45:40 +0200

    dynamic add row to layout

--- src/views/UserStory/components/Dashboard.jsx
+++ src/views/UserStory/components/Dashboard.jsx
@@ -101,7 +101,7 @@ class Dash extends Component {
           }]
         }],
       },
-      editMode: false,
+      editMode: true,
       isModalOpen: false,
       addWidgetOptions: null,
     };
@@ -200,6 +200,22 @@ class Dash extends Component {
     });
   }
 
+  addRow = () => {
+    let row = {
+          columns: [{
+            className: 'col-md-12 col-sm-12 col-xs-12',
+            widgets: [],
+          }],
+        }
+
+    let layout = this.state.layout
+    layout.rows.unshift(row);
+    this.setState({
+      layout: layout
+    });
+
+  }
+
   //       <Iframe iframe='<iframe width="600"  height="400" seamless frameBorder="0" scrolling="no" src="http://localhost:8088/superset/explore/table/3/?form_data=%7B%22datasource%22%3A%223__table%22%2C%22viz_type%22%3A%22line%22%2C%22slice_id%22%3A20%2C%22granularity_sqla%22%3A%22ds%22%2C%22time_grain_sqla%22%3A%22Time+Column%22%2C%22since%22%3A%22100+years+ago%22%2C%22until%22%3A%22now%22%2C%22metrics%22%3A%5B%22sum__num%22%5D%2C%22groupby%22%3A%5B%22name%22%5D%2C%22limit%22%3A%2225%22%2C%22timeseries_limit_metric%22%3Anull%2C%22show_brush%22%3Afalse%2C%22show_legend%22%3Atrue%2C%22rich_tooltip%22%3Atrue%2C%22show_markers%22%3Afalse%2C%22x_axis_showminmax%22%3Atrue%2C%22line_interpolation%22%3A%22linear%22%2C%22contribution%22%3Afalse%2C%22x_axis_label%22%3A%22%22%2C%22x_axis_format%22%3A%22smart_date%22%2C%22y_axis_label%22%3A%22%22%2C%22y_axis_bounds%22%3A%5Bnull%2Cnull%5D%2C%22y_axis_format%22%3A%22.3s%22%2C%22y_log_scale%22%3Afalse%2C%22rolling_type%22%3A%22None%22%2C%22time_compare%22%3Anull%2C%22num_period_compare%22%3A%22%22%2C%22period_ratio_type%22%3A%22growth%22%2C%22resample_how%22%3Anull%2C%22resample_rule%22%3Anull%2C%22resample_fillmethod%22%3Anull%2C%22where%22%3A%22%22%2C%22having%22%3A%22%22%2C%22filters%22%3A%5B%5D%7D&standalone=true&height=400"></iframe>' />
 
   render() {
@@ -207,7 +223,7 @@ class Dash extends Component {
     <Container>
       <AddWidgetDialog widgets={this.state.widgets} isModalOpen={this.state.isModalOpen} onRequestClose={this.onRequestClose} onWidgetSelect={this.handleWidgetSelection}/>
       <Header />
-      <EditBar onEdit={this.toggleEdit} />
+      <EditBar onEdit={this.toggleEdit} onAddRow={this.addRow}/>
       <Dashboard
         frameComponent={CustomFrame}
         onRemove={this.onRemove}
--- src/views/UserStory/components/EditBar.jsx
+++ src/views/UserStory/components/EditBar.jsx
@@ -1,6 +1,6 @@
 import React, { PropTypes } from 'react';
 
-const EditBar = ({ onEdit }) => {
+const EditBar = ({ onEdit, onAddRow }) => {
   return (
     <div className="row edit-bar">
       <div className="col-sm-12 text-right">
@@ -9,12 +9,24 @@ const EditBar = ({ onEdit }) => {
             Edit
           </button>
       </div>
+
+      <div className="row edit-bar-items">
+        <div className="col-sm-12 text-right">
+          <button type="button" className="btn btn-default btn-xs" onClick={onAddRow}>
+            <span className="glyphicon glyphicon-plus" aria-hidden="true"></span>
+              Add Row
+            </button>
+        </div>
+      </div>
+
     </div>
+
   );
 };
 
 EditBar.propTypes = {
   onEdit: PropTypes.func,
+  onAddRow: PropTypes.func,
 };
 
 export default EditBar;
