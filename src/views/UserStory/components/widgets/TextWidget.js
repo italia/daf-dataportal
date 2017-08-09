3ab8fd473c9addabea27c5cc2657f91bb6a04b10
Luca Carducci <l.carducci@almaviva.it>
2017-08-09 11:31:07 +0200

    save html

--- src/views/UserStory/components/widgets/TextWidget.js
+++ src/views/UserStory/components/widgets/TextWidget.js
@@ -27,18 +27,49 @@ class TextWidget extends Component {
     });
   };
 
+  save(){
+    //let html = draftToHtml(convertToRaw(this.state.editorState.getCurrentContent()));
+    //console.log(html)
+    //this.state.text = html;
+
+    this.setState({
+      edit: false
+    })
+  }
+
+  edit(){
+    this.setState({
+      edit: true
+    })
+  }
+
   render() {
     const { editorState } = this.state;
-    return (<div className="rdw-storybook-root">
-      <span>HTML Content: <pre>{ draftToHtml(convertToRaw(editorState.getCurrentContent())) }</pre></span>
-      <Editor
-        editorState={editorState}
-        toolbarClassName="rdw-storybook-toolbar"
-        wrapperClassName="rdw-storybook-wrapper"
-        editorClassName="rdw-storybook-editor"
-        onEditorStateChange={this.onEditorStateChange}
-      />
-    </div>);
+    return (
+    <div>
+        {!this.state.edit && 
+          <div>
+            { draftToHtml(convertToRaw(editorState.getCurrentContent())) }
+            <div className="mt-20">
+              <button onClick={() => this.edit()} type="button" className="btn btn-link" >Edit</button>
+            </div>
+          </div>
+        }
+        
+        {this.state.edit && 
+          <div>
+            <Editor
+              editorState={editorState}
+              toolbarClassName="rdw-storybook-toolbar"
+              wrapperClassName="rdw-storybook-wrapper"
+              editorClassName="rdw-storybook-editor"
+              onEditorStateChange={this.onEditorStateChange}
+            />
+            <button onClick={() => this.save()} type="button" className="btn btn-primary" >Save</button>
+          </div>
+        }
+    </div>
+    )
   }
 }
 
