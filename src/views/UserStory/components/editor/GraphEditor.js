411291bb3defef4a013c0e1ca128991779eef0a6
Luca Carducci <l.carducci@almaviva.it>
2017-08-11 16:39:42 +0200

    img & graph button

--- src/views/UserStory/components/editor/GraphEditor.js
+++ src/views/UserStory/components/editor/GraphEditor.js
@@ -10,11 +10,30 @@ class GraphEditor extends Component {
    * Render Function
    */
   render() {
+    
+    const iframeStyle = {
+      width: '100%',
+      height: '300px',
+      border: '0'
+    }
+
     return (
-    <div>
-        {this.props.graph}
-    </div>
-    );
+      <div>
+        <iframe
+          className={this.props.class}
+          ref="iframe"
+          frameBorder={'0'}
+          style={iframeStyle}
+          src={this.props.graph}
+        />
+        
+        <div className="text-center mt-20">
+          <button type="button" className="btn btn-default">
+              Change Graph
+          </button>
+        </div>
+      </div>
+    )
   }
 
 }
--- src/views/UserStory/components/editor/ImageEditor.js
+++ src/views/UserStory/components/editor/ImageEditor.js
@@ -1,9 +1,25 @@
 import React, { Component } from 'react';
 import Components from 'react';
+import TextEditor from './TextEditor';
 
 class ImageEditor extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      image: {
+        caption: props.image.caption,
+        url: props.image.url
+      }
+    }
+
+    this.onChange = this.onChange.bind(this);
+  }
+  
+  onChange = function(key, value) {
+    this.state.image[key] = value;
+    if(this.props.onChange)
+      this.props.onChange(this.props.keyValue, this.state.image);
   }
 
   /**
@@ -12,10 +28,21 @@ class ImageEditor extends Component {
   render() {
     return (
     <div>
-        {this.props.image.url}
-        <h5>
-          {this.props.image.caption}
-        </h5>
+        <img className="image-container" src={this.props.image.url} />
+        
+        <div className="text-center mt-20 mb-20">
+          <button type="button" className="btn btn-default">
+              Change Image
+          </button>
+        </div>
+
+        <TextEditor 
+          keyValue="caption"
+          text={this.props.image.caption} 
+          className="text-editor-img-caption"
+          onChange={this.onChange}
+          placeholder="Insert here image description..."
+        ></TextEditor>
     </div>
     );
   }
