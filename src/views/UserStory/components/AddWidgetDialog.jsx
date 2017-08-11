aebdd4665c064905bdea2658277ed68430d9f395
Luca Carducci <l.carducci@almaviva.it>
2017-08-11 11:30:12 +0200

    load preview

--- src/views/UserStory/components/AddWidgetDialog.jsx
+++ src/views/UserStory/components/AddWidgetDialog.jsx
@@ -1,11 +1,16 @@
 import React, { PropTypes } from 'react';
 import Modal from 'react-modal';
 import ReactDOM from 'react-dom';
+import $ from 'jquery';
 
 const AddWidgetDialog = ({ widgets, isModalOpen, onRequestClose, onWidgetSelect}) => {
   const widgetItems = Object.keys(widgets).map((widget, key) => {
     let wid = widgets[widget].type;
     
+    let onLoadIframe = function(id) {
+      $("#title-preview-" + id).hide();
+    }
+
     if(widget && widget =="TextWidget")
       return (
         <div key={key} className="col-sm-12 col-md-12 col-lg-12 list-group mb-20">
@@ -20,11 +25,11 @@ const AddWidgetDialog = ({ widgets, isModalOpen, onRequestClose, onWidgetSelect}
       return (
         <div key={key} className="col-sm-6 col-md-6 col-lg-6 list-group mb-20">
           <a href="#/dash" className="list-group-item" onClick={() => onWidgetSelect(widget)}>
-            <h6 className="list-group-item-heading">
+            <h6 className="list-group-item-heading" id={"title-preview-" + key}>
               {widgets[widget].title}
             </h6>
             <div className="preview-widget">
-              {React.createElement(wid, widgets[widget].props)}
+              {React.createElement(wid, {...widgets[widget].props, class: "no-click", onLoad: () => onLoadIframe(key)})}
             </div>
           </a>
         </div>
