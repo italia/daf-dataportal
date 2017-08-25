944ec1318aef47a7cbaca100995ff2d19255d6bd
Alessandro Ercolani <alessandro@teamdigitale.governo.it>
2017-08-25 22:28:29 +0200

    Deploy settings

--- src/config/serviceurl.js
+++ src/config/serviceurl.js
@@ -1,7 +1,10 @@
 export const serviceurl = {
   // DatasetBackend module configs
-  apiURLDatiGov: "http://localhost:9000", 
-  apiURLCatalog: "http://localhost:9001",
+ // apiURLDatiGov: "http://localhost:9000", 
+ // apiURLCatalog: "http://localhost:9001",
+ // apiURLIngestion: "http://localhost:9002",
+  apiURLDatiGov: "http://datipubblici.default.svc.cluster.local:9000", 
+  apiURLCatalog: "http://catalog-manager.default.svc.cluster.local:9000",
   apiURLIngestion: "http://localhost:9002",
   apiURL_dati_gov: "/dati-gov/v1",
   apiURL_mock: "http://localhost:3000/mock/",
