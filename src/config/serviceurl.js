ffe035c28ff7ce980d780e7be98d70a328d48526
raippl <raippl@yahoo.it>
2017-09-18 18:23:03 +0200

    serviceurl merged

--- src/config/serviceurl.js
+++ src/config/serviceurl.js
@@ -1,18 +1,5 @@
 export const serviceurl = {
   // DatasetBackend module configs
-<<<<<<< HEAD
-  /*apiURLDatiGov: "http://localhost:9000/dati-gov/v1", 
-  apiURLCatalog: "http://localhost:9001/catalog-manager/v1",
-  apiURLIngestion: "http://localhost:9002/ingestion-manager/v1",
-  apiURLSecurity: "http://localhost:9001/catalog-manager/v1",
-  */
-  //MOCK
-  /*apiURLDatiGov: "http://localhost:3001/dati-gov/v1", 
-  apiURLCatalog: "http://localhost:3001/catalog-manager/v1",
-  apiURLIngestion: "http://localhost:3001",
-  apiURLSecurity: "http://localhost:3001/catalog-manager/v1",
-*/
-=======
   //apiURLDatiGov: "http://localhost:9000/dati-gov/v1", 
   //apiURLCatalog: "http://localhost:9001/catalog-manager/v1",
   //apiURLIngestion: "http://localhost:9002/ingestion-manager/v1",
@@ -24,16 +11,11 @@ export const serviceurl = {
   //apiURLCatalog: "http://localhost:9001",
   //apiURLIngestion: "http://localhost:9002",
 
->>>>>>> cc7ef70917692070fd9f5196245f0f5ecf63ab44
   // PRODUCTION
   apiURLDatiGov: "http://datipubblici.default.svc.cluster.local:9000/dati-gov/v1", 
   apiURLCatalog: "http://catalog-manager.default.svc.cluster.local:9000/catalog-manager/v1",
   apiURLIngestion: "http://localhost:9002",
   apiURLSecurity : "http://catalog-manager.default.svc.cluster.local:9000/catalog-manager/v1",
-<<<<<<< HEAD
-=======
-
->>>>>>> cc7ef70917692070fd9f5196245f0f5ecf63ab44
   auth: "dGVzdDp0dWxsaWFlYmxp",
 
  "DatasetBackend": {
