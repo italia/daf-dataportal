3f42c40a4afd1328eb286721fe0f4baff367946d
Alessandro Ercolani <alessandro@teamdigitale.governo.it>
2017-10-16 09:36:49 +0200

    Serviceurl for deploy

--- src/config/serviceurl.js
+++ src/config/serviceurl.js
@@ -1,10 +1,10 @@
 export const serviceurl = {
   // DatasetBackend module configs
-  apiURLSSOManager: "http://localhost:9002/sso-manager",
-  apiURLDatiGov: "http://localhost:9000/dati-gov/v1", 
-  apiURLCatalog: "http://localhost:9001/catalog-manager/v1",
-  apiURLIngestion: "http://localhost:9002/ingestion-manager/v1",
-  apiURLSecurity: "http://localhost:9002/security-manager/v1",
+  //apiURLSSOManager: "http://localhost:9002/sso-manager",
+  //apiURLDatiGov: "http://localhost:9000/dati-gov/v1", 
+  //apiURLCatalog: "http://localhost:9001/catalog-manager/v1",
+  //apiURLIngestion: "http://localhost:9002/ingestion-manager/v1",
+  //apiURLSecurity: "http://localhost:9002/security-manager/v1",
 
   //MOCK
   //apiURLDatiGov: "http://localhost:3001/dati-gov/v1", 
@@ -23,10 +23,10 @@ export const serviceurl = {
  //urlJupiter: 'http://jupyterhub.default.svc.cluster.local:8000',
   
  // EXTERNAL
- //apiURLSSOManager: "https://api.daf.teamdigitale.it/sso-manager",
- //apiURLDatiGov: "https://api.daf.teamdigitale.it/dati-gov/v1", 
- //apiURLCatalog: "https://api.daf.teamdigitale.it/catalog-manager/v1",
- //apiURLSecurity : "https://api.daf.teamdigitale.it/security-manager/v1",
+ apiURLSSOManager: "https://api.daf.teamdigitale.it/sso-manager",
+ apiURLDatiGov: "https://api.daf.teamdigitale.it/dati-gov/v1", 
+ apiURLCatalog: "https://api.daf.teamdigitale.it/catalog-manager/v1",
+ apiURLSecurity : "https://api.daf.teamdigitale.it/security-manager/v1",
 
   urlMetabase: 'https://graph.daf.teamdigitale.it',
   urlSuperset: 'https://bi.daf.teamdigitale.it',
@@ -34,9 +34,9 @@ export const serviceurl = {
 
   auth: "dGVzdDp0dWxsaWFlYmxp",
   
-  //domain: ".daf.teamdigitale.it",
+  domain: ".daf.teamdigitale.it",
 
-  domain: "/",
+  //domain: "/",
 
  "DatasetBackend": {
     "Search": {
