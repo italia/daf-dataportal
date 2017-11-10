c07cebb3907e055dc113b729b386da0e1dd0905b
Pierpaolo Rai <raippl@yahoo.it>
2017-11-10 10:32:41 +0100

    Async cookie call in App.js

--- src/views/Login/Login.js
+++ src/views/Login/Login.js
@@ -12,6 +12,7 @@ import {
 } from 'react-modal-bootstrap';
 import { serviceurl } from '../../config/serviceurl.js'
 import { setCookie } from '../../utility'
+import OverlayLoader from 'react-overlay-loading/lib/OverlayLoader'
 
 function setErrorMsg(error) {
   return {
@@ -25,7 +26,8 @@ class Login extends Component {
     this.props = props;
     this.state = {
       isOpen: false,
-      loginMessage: null
+      loginMessage: null,
+      uploading: false
     }
   }
 
@@ -34,37 +36,42 @@ class Login extends Component {
   handleSubmit = (e) => {
     e.preventDefault()
     const { dispatch, selectDataset } = this.props
+    this.setState({uploading: true})
     dispatch(getAuthToken(this.email.value, this.pw.value))
       .then(json => {
         localStorage.setItem('token', json);
-        dispatch(setApplicationCookie('superset'))
-          .then(json => {
-            if (json) { //&& json.result) {
-              document.cookie = json.result + "; path=/; domain=" + serviceurl.domain;
+              setCookie('superset', json)
               dispatch(setApplicationCookie('metabase'))
                 .then(json => {
-                  if (json ) { //&& json.result) {
-                    document.cookie = json.result + "; path=/; domain=" + serviceurl.domain;
-                   // dispatch(setApplicationCookie('jupyter'))
-                   // .then(json => {
-                      if (json){ //&& json.result) {
-                        document.cookie = json.result + "; path=/hub/; domain=" + serviceurl.domain;
-                        dispatch(loginAction())
-                        .then(json => {
-                          dispatch(receiveLogin(json))
-                          dispatch(addUserOrganization(json.uid))
-                          this.props.history.push('/home')
-                        })
-                      }
-                   // })
+                  if (json) {
+                    setCookie('metabase', json)
+                    dispatch(setApplicationCookie('jupyter'))
+                      .then(json => {
+                        if (json) {
+                          setCookie('jupyter', json)
+                          dispatch(setApplicationCookie('grafana'))
+                            .then(json => {
+                              if (json) {
+                                setCookie('grafana', json)
+                                dispatch(loginAction())
+                                  .then(json => {
+                                    dispatch(receiveLogin(json))
+                                    dispatch(addUserOrganization(json.uid))
+                                    this.props.history.push('/home')
+                                  })
+                              }
+                            })
+                        }
+                      })
                   }
                 })
-            }
-          })
-      })
-      .catch((error) => {
-        this.setState(setErrorMsg('Invalid email/password.'))
-      })
+            })
+            .catch((error) => {
+              this.setState({
+                loginMessage: 'Nome utente o password non corretto.',
+                uploading: false
+              })
+            })
   }
 
   openModal = () => {
@@ -103,44 +110,61 @@ class Login extends Component {
         </Modal>
         <div className="row justify-content-center">
           <div className="col-md-8">
-            <div className="card-group mb-0">
-              <div className="card p-2">
-                <div className="card-block">
-                  <h1>Login</h1>
-                  <p className="text-muted">Accedi alla tua area personale</p>
-                  {this.state.loginMessage &&
-                    <div className="alert alert-danger" role="alert">
-                      {this.state.loginMessage}
-                    </div>
-                  }
-                  <div className="input-group mb-1">
-                    <span className="input-group-addon">
-                      <i className="icon-user"></i>
-                    </span>
-                    <input className="form-control" ref={(email) => this.email = email} placeholder="Email" />
+              <div className="card-group mb-0">
+                <div className="card p-2">
+                  <div className="card-block">
+                    <OverlayLoader 
+                      color={'blue'} 
+                      loader="PulseLoader" 
+                      text="Caricamento in corso..." 
+                      active={this.state.uploading} 
+                      backgroundColor={'grey'}
+                      
+                      >
+                    <h1>Login</h1>
+                    <p className="text-muted">Accedi alla tua area personale</p>
+                    {this.state.loginMessage &&
+                      <div className="alert alert-danger" role="alert">
+                        {this.state.loginMessage}
+                      </div>
+                    }
+                    
+                      <div className="input-group mb-1">
+                        <span className="input-group-addon">
+                          <i className="icon-user"></i>
+                        </span>
+                        <input className="form-control" ref={(email) => this.email = email} placeholder="Email" />
+                      </div>
+                    
+                    
+                      <div className="input-group mb-2">
+                        <span className="input-group-addon">
+                          <i className="icon-lock"></i>
+                        </span>
+                        <input type="password" className="form-control" placeholder="Password" ref={(pw) => this.pw = pw} />
+                      </div>
+                    
+                   
+                      <div className="row">
+                        <div className="col-6">
+                          <button type="button" className="btn btn-primary px-2" onClick={this.handleSubmit.bind(this)}>Login</button>
+                        </div>
+                        <div className="col-6 text-right"><button type="button" className="btn btn-link px-0" onClick={this.openModal}>Password dimenticata?</button>
+                        </div>
+                      </div>
+                    
+                </OverlayLoader>    
+              </div>
+                </div>
+                <div className="card card-inverse card-primary py-3 hidden-md-down">
+                  <div className="card-block text-center"><div>
+                    <h2>Iscriviti</h2>
+                    <p>Per accedere all'area riservata del portale occorre registrarsi qui: </p>
+                    <button type="button" className="btn btn-primary active mt-1" onClick={() => this.props.history.push('/register')} >Registrati!</button>
                   </div>
-                  <div className="input-group mb-2">
-                    <span className="input-group-addon">
-                      <i className="icon-lock"></i>
-                    </span>
-                    <input type="password" className="form-control" placeholder="Password" ref={(pw) => this.pw = pw} />
-                  </div><div className="row">
-                    <div className="col-6">
-                      <button type="button" className="btn btn-primary px-2" onClick={this.handleSubmit.bind(this)}>Login</button>
-                    </div><div className="col-6 text-right"><button type="button" className="btn btn-link px-0" onClick={this.openModal}>Password dimenticata?</button>
-                    </div>
                   </div>
                 </div>
               </div>
-              <div className="card card-inverse card-primary py-3 hidden-md-down">
-                <div className="card-block text-center"><div>
-                  <h2>Iscriviti</h2>
-                  <p>Per accedere all'area riservata del portale occorre registrarsi qui: </p>
-                  <button type="button" className="btn btn-primary active mt-1" onClick={() => this.props.history.push('/register')} >Registrati!</button>
-                </div>
-                </div>
-              </div>
-            </div>
           </div>
         </div>
       </div>
