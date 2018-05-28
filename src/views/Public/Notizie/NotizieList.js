e8569e3711ad298dfb979d180579496765973dec
Pierpaolo Rai <raippl@yahoo.it>
2018-05-28 14:07:38 +0200

    fix dash/stories search

--- src/views/Public/Notizie/NotizieList.js
+++ src/views/Public/Notizie/NotizieList.js
@@ -13,7 +13,7 @@ class NotizieList extends Component {
     componentDidMount(){
         // ORIG URL  https://medium.com/feed/team-per-la-trasformazione-digitale
         // FINAL URL https://datipubblici.daf.teamdigitale.it/dati-gov/medium/medium.com/feed/team-per-la-trasformazione-digitale
-        let url = serviceurl.apiMedium + this.props.properties.url
+        let url = serviceurl.apiMedium + this.props.properties.mediumURL
         this.load(url);
     }
 
