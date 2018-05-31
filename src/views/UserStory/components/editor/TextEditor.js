d38c4162a637f03f410b6f2ba0197d419a15ecd8
CrisTofani <cri.tofani@gmail.com>
2018-05-31 17:41:17 +0200

    Userstory view redesigned

--- src/views/UserStory/components/editor/TextEditor.js
+++ src/views/UserStory/components/editor/TextEditor.js
@@ -38,7 +38,7 @@ class TextEditor extends Component {
    */
   render() {
     return (
-    <div className={"text-editor " + this.props.className}>
+    <div className={"text-left mx-auto text-editor " + this.props.className}>
       {
         this.props.readonly!=true &&
 
