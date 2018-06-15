40f9fff0511160c506001f2b8344b3337436c84e
and0111 <and.simonetta@gmail.com>
2018-06-15 12:30:01 +0200

    Updated DataApplicationsList:
    - inline styled fonts..
    - padding bottom..
    - component array key mapping..
    - link to dataApps

--- src/views/Public/DataApplication/components/DataApplicationsList.jsx
+++ src/views/Public/DataApplication/components/DataApplicationsList.jsx
@@ -12,27 +12,37 @@ import {
 } from 'reactstrap'
 
 const DataApplication = props => {
-  const { title, author, previewBase64, description, sharingLinks } = props
+  const { applicationURL, title, author, previewBase64, description, sharingLinks } = props
   return (
-    <Jumbotron className="alert-secondary">
+    <Jumbotron className="alert-secondary pb-4">
       <Row>
         <Col sm={8}>
-          <h1 className="display-4" >{title}</h1>
+          <a href={applicationURL}>
+            <h1
+              className="display-4"
+              style={{
+                font: '700 40px/48px Titillium Web',
+                color: 'rgb(21, 27, 30)'
+              }}
+            >
+              {title}
+            </h1>
+          </a>
           <p className="lead">{author}</p>
           <hr />
-          <p>{description}</p>
+          <p style={{ font: '400 21px/32px Titillium Web' }}>{description}</p>
           <hr />
           <Nav>
-        <NavLink href={sharingLinks.github}>
-          <i className="fab fa-github fa-lg" /> Vai al codice sorgente
+            <NavLink href={sharingLinks.github}>
+              <i className="fab fa-github fa-lg" /> Vai al codice sorgente
         </NavLink>
-        <NavLink href={sharingLinks.medium}>
-          <i className="fab fa-medium fa-lg" /> Vai al post su Medium
+            <NavLink href={sharingLinks.medium}>
+              <i className="fab fa-medium fa-lg" /> Vai al post su Medium
         </NavLink>
-      </Nav>
+          </Nav>
         </Col>
         <Col sm={4}>
-          <CardImg   
+          <CardImg
             src={`data:image/jpg;base64,${previewBase64}`}
             alt={`Data Application ${title}`}
           />
@@ -43,7 +53,7 @@ const DataApplication = props => {
 }
 
 const mapDataApplications = dataApps =>
-  dataApps.map(dataApp => <DataApplication {...dataApp} />)
+  dataApps.map((dataApp, index) => <DataApplication key={index} {...dataApp} />)
 
 const createDataApplications = dataApps => (
   <Container className="pt-4">{mapDataApplications(dataApps)}</Container>
