00159ae80f3e0cbb9aa7f9e88371e577e07227ec
and0111 <and.simonetta@gmail.com>
2018-07-02 13:24:05 +0200

    Update API's URLs
    - Updated 'commonBaseURL'
    - Udated 'kataLOD' API URL

--- src/semantics/config/apiURLs.js
+++ src/semantics/config/apiURLs.js
@@ -1,28 +1,24 @@
-const commonBaseURL = `http://${
-  window.location.hostname === 'localhost'
-    ? 'localhost'
-    : 'dafdocker.westeurope.cloudapp.azure.com'
-}`
+const commonBaseURL = "https://api.daf.teamdigitale.test/daf-semantic-katalod";
 
 export const apiURLs = {
-  kataLOD: `${commonBaseURL}:7777/kb/api/v1`,
-  semanticValidator: `${commonBaseURL}:9000/validator`,
-  // LODView: `${commonBaseURL}:8080/lodview`,
-  LODE: `${commonBaseURL}:9090/lode/extract?url=`,
-  WebVOWL: `${commonBaseURL}:8080/webvowl/#iri=`
-}
+  kataLOD: `${commonBaseURL}/kb/api/v1` //,
+  // semanticValidator: `${commonBaseURL}:9000/validator`,
+  // // LODView: `${commonBaseURL}:8080/lodview`,
+  // LODE: `${commonBaseURL}:9090/lode/extract?url=`,
+  // WebVOWL: `${commonBaseURL}:8080/webvowl/#iri=`
+};
 
 export const jsonRequest = {
-  method: 'GET',
+  method: "GET",
   headers: new Headers({
-    'Content-Type': 'application/json',
-    Accept: 'application/json'
+    "Content-Type": "application/json",
+    Accept: "application/json"
   }),
-  mode: 'cors',
-  cache: 'no-cache'
-}
+  mode: "cors",
+  cache: "no-cache"
+};
 
 export const validationFormRequest = formData => ({
-  method: 'POST',
+  method: "POST",
   body: formData
-})
+});
