cc65f5046b14a30dd410027a098541e8e587cf33
CrisTofani <cri.tofani@gmail.com>
2018-09-28 16:07:00 +0200

    Minor fixes; redesigned registration page added recaptcha

--- src/views/Login/Login.js
+++ src/views/Login/Login.js
@@ -275,7 +275,7 @@ class Login extends Component {
                           <input type="submit" className="btn btn-primary px-2" onClick={this.handleSubmit.bind(this)} value="Login"/>
                         </div>
                         <div className="col-6 text-right" style={{ position: 'initial' }}>
-                        <button type="button" className="btn btn-link px-0" onClick={() => this.props.history.push('/requestreset')}>Password dimenticata?</button>
+                        <button type="button" className="btn btn-link text-primary px-0" onClick={() => this.props.history.push('/requestreset')}>Password dimenticata?</button>
                         </div>
                       </div>
                     </form>
