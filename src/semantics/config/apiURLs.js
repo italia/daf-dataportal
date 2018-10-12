668f8f2721f811e0bfc242d140d70933fdfde26f
Pierpaolo Rai <raippl@yahoo.it>
2018-10-12 18:07:01 +0200

    merge

--- src/semantics/config/apiURLs.js
+++ src/semantics/config/apiURLs.js
@@ -1,12 +1,8 @@
 import { serviceurl } from "../../config/serviceurl";
 
-
 export const apiURLs = {
   kataLOD: serviceurl.urlKatalod,
   semanticValidator: serviceurl.urlSemanticValidator
-  // // LODView: `${commonBaseURL}:8080/lodview`,
-  // LODE: `${commonBaseURL}:9090/lode/extract?url=`,
-  // WebVOWL: `${commonBaseURL}:8080/webvowl/#iri=`
 };
 
 export const jsonRequest = {
