44591a032297f4d5220902b908ae2df216324877
Cristiano Tofani <cri.tofani@gmail.com>
2019-03-11 10:46:04 +0100

    Merge

--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -10,11 +10,7 @@ module.exports =  {
     //https: true,
     // contentBase: 'dist',
     port: 80,
-<<<<<<< HEAD
     host: 'localhost.dataportal.daf.teamdigitale.test'
-=======
-    host: 'localhost.dataportal.daf.teamdigitale.it'
->>>>>>> 7481dc31943d8b042c0fec7e8a95b0a32b13cdbd
   },
   watch: true,
   plugins: [
