a82c6b8a57780e7f575aac989c419a96742d7769
Trosino Alessandro <A.Trosino@almaviva.it>
2019-03-19 15:34:28 +0100

    DATPOR-582 fix new languages system

--- src/views/Messages/services/MessageService.js
+++ src/views/Messages/services/MessageService.js
@@ -139,7 +139,17 @@ export default class MessageService {
                 'Authorization': 'Bearer ' + localStorage.getItem('token')
             },
             body: JSON.stringify(message)
-        })
+        }).then(function(response) {
+            if (response.status === 200) {
+                return response;
+            } else if (response.status === 401) {
+                throw new Error( messages.validazione.utenteNoAdmin );
+            } else if (response.status === 500) {
+                throw new Error( messages.validazione.erroreGenerico );
+            }else{
+                throw new Error(response.statusText);  
+            }
+          })
         return response;
     }
 
@@ -155,7 +165,17 @@ export default class MessageService {
                 'Authorization': 'Bearer ' + localStorage.getItem('token')
             },
             body: JSON.stringify(message)
-        })
+        }).then(function(response) {
+            if (response.status === 200) {
+                return response;
+            } else if (response.status === 401) {
+                throw new Error( messages.validazione.utenteNoAdmin );
+            } else if (response.status === 500) {
+                throw new Error( messages.validazione.erroreGenerico );
+            }else{
+                throw new Error(response.statusText);  
+            }
+          })
         return response;
     }
 
@@ -169,8 +189,17 @@ export default class MessageService {
                 'Content-Type': 'application/json',
                 'Authorization': 'Bearer ' + localStorage.getItem('token')
             }
-        })
-
+        }).then(function(response) {
+            if (response.status === 200) {
+                return response;
+            } else if (response.status === 401) {
+                throw new Error( messages.validazione.utenteNoAdmin );
+            } else if (response.status === 500) {
+                throw new Error( messages.validazione.erroreGenerico );
+            }else{
+                throw new Error(response.statusText);  
+            }
+          })
         return response;
     }
 
@@ -184,8 +213,17 @@ export default class MessageService {
                 'Content-Type': 'application/json',
                 'Authorization': 'Bearer ' + localStorage.getItem('token')
             }
-        })
-
+        }).then(function(response) {
+            if (response.status === 200) {
+                return response;
+            } else if (response.status === 401) {
+                throw new Error( messages.validazione.utenteNoAdmin );
+            } else if (response.status === 500) {
+                throw new Error( messages.validazione.erroreGenerico );
+            }else{
+                throw new Error(response.statusText);  
+            }
+          })
         return response;
     }
 
@@ -199,8 +237,17 @@ export default class MessageService {
                 'Content-Type': 'application/json',
                 'Authorization': 'Bearer ' + localStorage.getItem('token')
             }
-        })
-
+        }).then(function(response) {
+            if (response.status === 200) {
+                return response;
+            } else if (response.status === 401) {
+                throw new Error( messages.validazione.utenteNoAdmin );
+            } else if (response.status === 500) {
+                throw new Error( messages.validazione.erroreGenerico );
+            }else{
+                throw new Error(response.statusText);  
+            }
+          })
         return response;
     }
 
