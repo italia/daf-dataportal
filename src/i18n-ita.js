db4b35908a1d9f169a255a9b2c69d84c8721ed5e
Biagio Antonucci <antonucci.biagio14@gmail.com>
2019-03-27 15:33:16 +0100

    Completed getting started guide

--- src/i18n-ita.js
+++ src/i18n-ita.js
@@ -1,190 +1,215 @@
 export const messages = {
-            menu    :   {
-                home            :   "Home",
-                dataset         :   "Dataset",
-                widget          :   "Widget",    
-                dashboard       :   "Dashboard",    
-                storie          :   "Storie",    
-                toolkit         :   "TOOLKIT",    
-                crea            :   "Crea",    // ---
-                nuovoDataset    :   "Nuovo Dataset",    
-                nuovaDashboard  :   "Nuova Dashboard",    
-                nuovaStoria     :   "Nuova Storia",
-                strumenti       :   "Strumenti",  // ----  
-                superset        :   "Superset",
-                open            :   "Open",
-                privato         :   "Privato",
-                semantica       :   "Semantica",//----
-                ontologie       :   "Ontologie",
-                vocabolari      :   "Vocabolari",
-                validatore      :   "Validatore",
-                info            :   "INFO",
-                documentazione  :   "Documentazione",
-                amministrazione :   "Amministrazione",
-                gestioneUtenti  :   "Gestione Utenti",
-                organizzazioni  :   "Organizzazioni",
-                interfaccia     :   "Interfaccia",
-                messaggi        :   "Messaggi",
-                gestioneMessaggi:   "Gestione Messaggi",
-                gestioneTTL     :   "Gestione TTL"
-            },
-            label   :   {
-                modifica        :   "Modifica",    
-                Success         :   "Success",
-                Error           :   "Error",
-                Info            :   "Info",
-                System          :   "System",
-                salvataggio     :   "Salvataggio",
-                salvataggioOK   :   "Salvataggio effettuato con successo",
-                errore          :   "Errore",
-                salvataggioKO   :   "Impossibile effettuare il salvataggio",
-                gettingStarted  :    "Getting started"
-            },
-            validazione : {
-                campoObbligatorio       :   "Campo obbligatorio",
-                soloNumeri              :   "Solo Numeri"
-            },
+    menu    :   {
+        home            :   "Home",
+        dataset         :   "Dataset",
+        widget          :   "Widget",    
+        dashboard       :   "Dashboard",    
+        storie          :   "Storie",    
+        toolkit         :   "TOOLKIT",    
+        crea            :   "Crea",    // ---
+        nuovoDataset    :   "Nuovo Dataset",    
+        nuovaDashboard  :   "Nuova Dashboard",    
+        nuovaStoria     :   "Nuova Storia",
+        strumenti       :   "Strumenti",  // ----  
+        superset        :   "Superset",
+        open            :   "Open",
+        privato         :   "Privato",
+        semantica       :   "Semantica",//----
+        ontologie       :   "Ontologie",
+        vocabolari      :   "Vocabolari",
+        validatore      :   "Validatore",
+        info            :   "INFO",
+        documentazione  :   "Documentazione",
+        amministrazione :   "Amministrazione",
+        gestioneUtenti  :   "Gestione Utenti",
+        organizzazioni  :   "Organizzazioni",
+        interfaccia     :   "Interfaccia",
+        messaggi        :   "Messaggi",
+        gestioneMessaggi:   "Gestione Messaggi",
+        gestioneTTL     :   "Gestione TTL"
+    },
+    label   :   {
+        modifica        :   "Modifica",    
+        Success         :   "Success",
+        Error           :   "Error",
+        Info            :   "Info",
+        System          :   "System",
+        salvataggio     :   "Salvataggio",
+        salvataggioOK   :   "Salvataggio effettuato con successo",
+        errore          :   "Errore",
+        salvataggioKO   :   "Impossibile effettuare il salvataggio",
+        gettingStarted  :   "Getting started con la Piattaforma Digitale Nazionale Dati (PDND)"
+    },
+    validazione : {
+        campoObbligatorio       :   "Campo obbligatorio",
+        soloNumeri              :   "Solo Numeri"
+    },
 
-    lineeGuida: [
+gettingStarted: [
 
-        {
-            step: 0,
-            titolo: 'Step 1 - Scopri la piattaforma digitale nazionale dati.',
-            // descrizione: '&lt;h1&gt;Hi there!&lt;/h1&gt;'
-            descrizione: `La piattaforma PDND è aperta a tutti: pa, cittadini e imprese.
-            Qui puoi consultare un elenco di risorse sulla PDND (partire da questo …)
-            Iscriviti qui
-            <a href="https://dataportal.daf.teamdigitale.it/#/register"> https://dataportal.daf.teamdigitale.it/#/register </a>
-            e inizia ad esplorare le potenzialità del daf.`
-
-
-        },
-
-        {
-            step: 1,
-            titolo: 'Step 2 - Segnalaci il tuo interesse',
-            descrizione: `Sei un’amministrazione pubblica o un ente di ricerca e vuoi iniziare ad utilizzare la Piattaforma Digitale Nazionale Dati per condividere dati con altri enti che già la utilizzano o all’interno della tua stessa organizzazione?
- 
-
-            • Invia una mail all’indirizzo:  <a href="mailto:pdnd@teamdigitale.governo.it"> pdnd@teamdigitale.governo.it </a> specificando queste informazioni:
-                ◦ Subject:  “Adesione PDND”
-        
-        Nel corpo della mail indicare:
-                ◦ Nome Ente - dipartimento 
-                ◦ Indirizzo
-                ◦ Nome e Cognome referente 
-                ◦ Qualifica referente
-                ◦ Indirizzo mail referente
-                ◦ Breve descrizione sull’utilizzo che si intende fare della PDND
-                ◦ Conosci già la PDND? Si, no
-                ◦ Il tuo ente espone un portale open data? Si, no`
-        },
+{
+    step: 0,
+    titolo: 'Scopri la PDND',
+    descrizione: `La Piattaforma Digitale Nazionale Dati fa parte delle attività previste dal <a href = "https://docs.italia.it/italia/piano-triennale-ict/pianotriennale-ict-doc/it/2019-2021/05_dati-della-pubblica-amministrazione.html#piattaforma-digitale-nazionale-dati-pdnd"> Piano Triennale ICT </a>
+    
+    atte a valorizzare il patrimonio informativo pubblico nazionale; in quanto tale è aperta a tutti: in primo luogo alle Pubbliche Amministrazioni, ma anche a singoli cittadini, università, enti di ricerca e imprese. L’obiettivo è creare un unico punto di accesso al mondo dei dati della Pubblica Amministrazione.
+    Fanne parte anche tu! Utilizza il seguente link per effettuare la registrazione  <a href="https://dataportal.daf.teamdigitale.it/#/register">https://dataportal.daf.teamdigitale.it/#/register</a>  
+    e inizia ad esplorare le potenzialità della PDND.<br></br>
+    
+    <i> Nota:</i> Se sei un funzionario di una PA utilizza il tuo indirizzo e-mail istituzionale; in questo modo potrai successivamente richiedere di essere associato al dominio della tua organizzazione quando verrà aggiunta alla PDND.
+     <br></br>
+   <b> Per saperne di più:</b> <a href="https://docs.italia.it/italia/daf/lg-patrimonio-pubblico/it/stabile/index.html#"> Linee guida nazionali per la valorizzazione del patrimonio informativo pubblico</a>`
 
 
+},
 
-        {
-            step: 2,
+{
+    step: 1,
+    titolo: 'Segnalaci il tuo interesse',
+    descrizione: `Fai parte di una Pubblica Amministrazione o di un Ente di Ricerca? Vorresti iniziare ad utilizzare la PDND per pubblicare, 
+    condividere e analizzare i dati all’interno della tua Amministrazione o con altri Enti che già la utilizzano?<br></br>
 
-            titolo: 'Step 3 - Conosciamoci',
-            descrizione: `Concordiamo una webcall per confrontarci su come proseguire, 
-            su che tipologia di dati intendete lavorare e se intendete condividere dei dataset con altre organizzazioni.
-            In questa sessione vi verrà fatta una demo della piattaforma e delle sue funzionalità`
-        },
-
-
-        {
-            step: 3,
-
-
-            titolo: 'Step 4 - Creiamo la tua organizzazione',
-            descrizione: `Una volta firmato il protocollo d’intesa, provvederemo a definire la tua organizzazione sulla PDND, 
-    questo ti permetterà di creare dei gruppi di lavoro a cui aggiungere gli utenti della tua organizzazione e 
-    condividere privatamente i dataset che andretecon loro.`
-
-
-        },
-
-        {
-            step: 4,
-
-            titolo: 'Step 5 - Popoliamo la tua organizzazione',
-            descrizione: ``
+    Puoi iniziare a esplorare la PDND consultando una prima lista di <a href="https://docs.italia.it/italia/daf/pianotri-elencobasidatichiave/it/stabile/">dataset chiave di interesse nazionale</a>; 
+    poi puoi inviare una mail all’indirizzo: <a href="mailto:pdnd@teamdigitale.governo.it?subject=Richiesta Adesione PDND"> pdnd@teamdigitale.governo.it </a> specificando queste informazioni:
+    
+    <br></br>
+    
+    Oggetto: “Richiesta Adesione PDND”
+    <br></br>
+Nel corpo della mail indicare:
+<li>  Nome Ente - dipartimento </li>
+<li>  Indirizzo </li>
+<li>  Nome e Cognome referente </li> 
+<li>  Qualifica referente </li>
+<li>  Indirizzo mail referente </li>
+<li>  Breve descrizione sull’utilizzo che si intende fare della PDND </li>
+<li>  Conosci già la PDND? Si, no </li> 
+<li>  Il tuo ente espone un portale open data? Si, no </li>
+<br></br>
+<b>Per saperne di più:</b> <a href="https://docs.italia.it/italia/daf/pianotri-schede-bdin/it/stabile/">Schede informative basi di dati di interesse nazionale</a>`
 
 
-        },
-
-        {
-            step: 5,
-
-            titolo: 'Step 6 - Indica il tuo utente editor',
-            descrizione: `Loggati con il tuo utente amministratore e promuovi ad editor l’utente prescelto per il ruolo.
-    Ricorda che l’utente editor è l’unico che può creare dei nuovi flussi e dataset per la tua organizzazione. 
-    Sara lui a doversi occupare di caricare i dati, una volta terminata la fase di metadatazione.`
-
-        },
-
-        {
-            step: 6,
-
-            titolo: 'Step 7 - Scambio chiave SSH',
-            descrizione: `Al fine di poter inserire nella PDND dei flussi dati tramite FTP è necessario validare l’identità di chi carica i dati. 
-    Per questo motivo devi fornirci una chiave pubblica SSH che utilizzerai con la tua utenza editor quando vorrai inviarci dei dataset.`
-
-        },
-
-        {
-            step: 7,
-
-            titolo: 'Step 8 - Crea il tuo primo flusso dati',
-            descrizione: `Al fine di stabilire un flusso che gestisca e renda fruibile un dataset è necessario eseguire due operazioni:
-
-            • METADATAZIONE: indicare alla PDND le caratteristiche del flusso legato al tuo dataset: attraverso il form di ingestion indica al sistema quali sono le informazioni contenute e le modalità cui cui verrà caricato
-            • CARICAMENTO DATI: caricare il contenuto del dataset che hai precedentemente metadato secondo le modalità scelte nel form di ingestion
-        
-        Nota bene: i dati che intendi caricare devono rispettare i casi d’uso esplicitati nel protocollo d’intesa. Non si devono inserire dati personali.
-        <a href = "https://docs.italia.it/italia/daf/daf-dataportal-it-docs/it/bozza/dataportal-privato/dataset.html"> https://docs.italia.it/italia/daf/daf-dataportal-it-docs/it/bozza/dataportal-privato/dataset.html <a/>
-        `
-
-        },
-
-        {
-            step: 8,
-
-            titolo: 'Step 9 - Utilizza i tuoi dati',
-            descrizione: `Una volta che i tuoi dataset sono stati metadatati e caricati puoi iniziare ad analizzare i tuoi dati 
-            attraverso gli strumenti interni ed esterni di analisi.
-
-            <a href="https://docs.italia.it/italia/daf/daf-dataportal-it-docs/it/bozza/dataportal-privato/dashboard.html"> https://docs.italia.it/italia/daf/daf-dataportal-it-docs/it/bozza/dataportal-privato/dashboard.html </a>
-            
-          <a href="https://docs.italia.it/italia/daf/daf-dataportal-it-docs/it/bozza/dataportal-privato/api.html"> https://docs.italia.it/italia/daf/daf-dataportal-it-docs/it/bozza/dataportal-privato/api.html </a>
-        `
-
-        },
+},
 
 
+{
+    step: 2,
 
-        {
-            step: 9,
-
-            titolo: 'Step 10 - Condividi i tuoi dati',
-            descrizione: ` Di default il dataset non è visibile a nessuno tranne all’utente editor che lo ha metadatato e caricato.
-            E’ necessario abilitare manualmente la pubblicazione del dataset e la condivisione all’interno dei tuoi workgroup 
-            dell’organizzazione.
-            Nel caso si voglia condividere uno o più dataset con un’altra Pubblica Amministrazione è necessario stipulare una 
-            convenzione tra le parti. A livello di sistema,  sarà cura del Referente Tecnico della Pubblica 
-            Amministrazione Titolare dei dati attuare la configurazione necessaria, seguendo le procedure di condivisione. `
-
-        },
-
-        {
-            step: 10,
-
-            titolo: 'Step 11 - Segnalaci eventuali malfunzionamenti o features',
-            descrizione: `Indicare come segnalarci bug,
-            Github`
-
-        },
+    titolo: 'Conosciamoci',
+    descrizione: `Ricevuta la richiesta, concorderemo una webcall con il vostro <a href="https://docs.italia.it/italia/piano-triennale-ict/pianotriennale-ict-doc/it/2019-2021/11_governare-la-trasformazione-digitale.html#il-responsabile-della-transizione-alla-modalita-digitale">Responsabile per la Transizione al Digitale</a> 
+    (e possibilmente con chi si occupa dei dati) 
+    della tua Amministrazione per confrontarci su come proseguire. Valuteremo la tipologia di dati sui quali intendete lavorare e la volontà di  condividere parte di questi  
+    con altre Amministrazioni o di pubblicarli in toto o in parte.
+    <br></br>
+    In questa sessione potremmo organizzare una dimostrazione della PDND e delle sue funzionalità. Successivamente prenderemo contatti con il vostro <i>Data Protection Officer</i> 
+    al fine di giungere alla definizione di un protocollo d’intesa tra l’Amministrazione e il Team per la Trasformazione Digitale, quale strumento giuridico per consentire 
+    l’impiego della PDND.
+    <br></br>
+    <!-- <b>Per saperne di più:</b> Template del protocollo di intesa -->` 
+   
+},
 
 
-    ]
+{
+    step: 3,
+
+
+    titolo: 'Creiamo la tua organizzazione',
+    descrizione: `Una volta firmato il protocollo d’intesa, provvederemo a definire sulla PDND un’organizzazione associata alla tua Amministrazione.
+
+    Due o più utenze fungeranno da amministratori e potranno gestire l’aggiunta e la rimozione di utenti o di gruppi di lavoro (workgroup); i workgroup potranno modellare gli uffici, 
+    i dipartimenti o altre strutture (permanenti o temporanee) della tua organizzazione. I dataset potranno essere condivisi privatamente a questo livello o con l’intera organizzazione.
+    <br></br>
+    <img src="" alt="">
+    <br></br>
+    <!-- <b> Per saperne di più: </b> Manuale utente - Funzionalità per tipologie di utente -->`
+
+
+},
+
+
+{
+    step: 4,
+
+    titolo: 'Definiamo gli utenti editor',
+    descrizione: `Alcuni utenti, detti <i>editor</i>, saranno autorizzati a definire nuovi dataset e a caricarli sulla Piattaforma. 
+    Questi utenti potranno attivare la condivisione o la pubblicazione di dataset, secondo specifiche procedure autorizzative.
+    <br></br>
+   <b> Per saperne di più:</b> <a href="https://docs.italia.it/italia/daf/daf-dataportal-it-docs/it/bozza/dataportal-privato/dataset.html">Manuale utente - Gestione dei dataset</a>`
+
+},
+
+
+{
+    step: 5,
+
+    titolo: 'Creiamo i tuoi primi flussi dati',
+    descrizione: `Ogni dataset presente all’interno della PDND deve essere innanzitutto collegato a un flusso di <i>ingestion</i>, che permetterà l’aggiornamento dei dati nel tempo.
+    A tal fine è necessario eseguire due operazioni:
+    
+    <br></br>
+    <li>METADATAZIONE:  procedura da eseguire una <i>tantum</i> per indicare alla PDND la struttura del dataset e le caratteristiche del flusso di alimentazione</li>
+    <li>CARICAMENTO DATI: caricare il contenuto del dataset precedentemente metadato secondo le modalità scelte</li>
+    <br></br>
+    I dataset creati saranno automaticamente conformi al profilo DCAT-AP_it.
+    <br></br>
+   <i> Nota:</i> i dati da caricare devono rispettare i casi d’uso esplicitati nel protocollo d’intesa ed essere conformi alla normativa vigente (GDPR).
+   <br></br>
+   <b>Per saperne di più:</b> <a href="https://docs.italia.it/italia/daf/linee-guida-cataloghi-dati-dcat-ap-it/it/stabile/">Linee guida per i cataloghi dati</a>`
+},
+
+{
+    step: 6,
+
+    titolo: 'Condividi i dati',
+    descrizione: `Di default il dataset non è visibile a nessuno tranne all’utente che lo ha creato fin quando non viene condiviso all’interno o all’esterno della tua organizzazione. 
+    Il proprietario del dataset è l’unico che potrà dare inizio al processo di condivisione, secondo specifiche procedure autorizzative.
+    <br></br>
+    Incoraggiamo la condivisione dei dataset con altre Amministrazioni, ma per poterla realizzare è necessario stipulare preventivamente un protocollo d’intesa formale tra le parti 
+    sulla base della normativa vigente. Completata questa fase, la condivisione sarà un gioco da ragazzi, la PDND penserà a tutto dietro le quinte!
+    <br></br>
+    <!-- <b>Per saperne di più:</b> Regole convenzioni + gestione su PDND -->`
+
+},
+
+
+
+{
+    step: 7,
+
+    titolo: 'Utilizza i dati',
+    descrizione: `Una volta che i dataset sono stati metadatati e caricati puoi finalmente utilizzarli per:
+   <ol>
+    <li>creare dei grafici con gli strumenti integrati come Superset</li>
+    <li>produrre delle <a href="https://docs.italia.it/italia/daf/daf-dataportal-it-docs/it/bozza/dataportal-privato/dashboard.html">data story</a> per raccontare i dati</li>
+    <li>accedere ai dati dall’esterno, sfruttando le <a href="https://docs.italia.it/italia/daf/daf-dataportal-it-docs/it/bozza/dataportal-privato/api.html">API</a> disponibili automaticamente</li>
+    <li>se sei editor, definire nuovi dataset derivati da quelli già presenti</li>
+    </ol>
+    <br></br>
+
+    La PDND garantisce l’allineamento dei dati su tutte le applicazioni e interfacce integrate.
+    <br></br>
+
+    <!-- <b>Per saperne di più: </b> -->`
+},
+
+{
+    step: 8,
+
+    titolo: 'Vuoi contribuire al progetto?',
+    descrizione: `La Piattaforma Digitale Nazionale dei Dati è in continua evoluzione: abbiamo degli obiettivi ambiziosi e ci serve il contributo di tutti per raggiungerli. 
+    Vuoi darci una mano? 
+    <br></br>
+    <li>  aprendo una issue su <a href="https://github.com/italia/?utf8=%E2%9C%93&q=daf&type=&language=">uno dei repository GitHub</a> </li>
+    <li>  scrivendo sul <a href="https://forum.italia.it/c/daf">Forum Italia </a></li>
+    <li>  canale <a href="https://developersitalia.slack.com/messages/C760XQX9Q">#daf</a> sullo <a href="https://slack.developers.italia.it/">Slack di Developers Italia </a> </li> 
+    <li>  scrivendo una e-mail all’indirizzo <a href="mailto:pdnd@teamdigitale.governo.it">pdnd@teamdigitale.governo.it.</a> </li>
+    <br></br>
+    <b>Per saperne di più:</b> <a href="https://docs.italia.it/italia/daf/daf-piano-di-sviluppo/it/bozza/">Piano di sviluppo</a> e <a href="https://docs.italia.it/italia/daf/daf-docs/en/bozza/">Developer Documentation</a>`
+
+},
+
+
+]
+
 }; 
